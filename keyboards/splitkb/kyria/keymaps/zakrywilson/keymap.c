460bc52d4566d5570f888b47c21178cfa2c70459
Zach Wilson <zachjwilson@google.com>
2022-07-18 08:29:56 -0500

    Fixed some issues

--- keyboards/splitkb/kyria/keymaps/zakrywilson/keymap.c
+++ keyboards/splitkb/kyria/keymaps/zakrywilson/keymap.c
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `-----------------------------'  `-----------------------------'
  */
 [COLEMAK] = LAYOUT(
-    KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_G,                                       KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_MINS,
+    KC_TAB,  KC_Q, KC_W, KC_F, KC_P, KC_G,                                      KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_MINS,
     QK_GESC, KC_A, KC_R, KC_S, KC_T, KC_D,                                      KC_H, KC_N, KC_E, KC_I, KC_O, KC_QUOT,
     KC_BSLS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_BSPC, KC_DEL,   KC_DEL, KC_ENT,   KC_K, KC_M, KC_COMM, KC_DOT, LT(MOVE, KC_SLSH), KC_EQL,
     KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_ENT,               KC_BSPC, TT(SYM), KC_SPC, MO(MOVE), MO(FUN)),
@@ -134,10 +134,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `-----------------------------'  `-----------------------------'
  */
 [QWERTY] = LAYOUT(
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                      KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
-    QK_GESC, KC_A, KC_S, KC_D, KC_F, KC_G,                                     KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-    KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_ENT,              KC_BSPC, TT(SYM), KC_SPC, MO(MOVE), MO(FUN)),
-    KC_BSLS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_ENT, KC_DEL,  KC_DEL, KC_ENT,   KC_N, KC_M, KC_COMM, KC_DOT, LT(MOVE, KC_SLSH), KC_EQL,
+    KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,                                      KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
+    QK_GESC, KC_A, KC_S, KC_D, KC_F, KC_G,                                      KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+    KC_BSLS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_BSPC, KC_DEL,   KC_DEL, KC_ENT,   KC_K, KC_M, KC_COMM, KC_DOT, LT(MOVE, KC_SLSH), KC_EQL,
+    KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_ENT,               KC_BSPC, TT(SYM), KC_SPC, MO(MOVE), MO(FUN)),
 
 /*
  * Keymap 2: Symbols & Numbers
