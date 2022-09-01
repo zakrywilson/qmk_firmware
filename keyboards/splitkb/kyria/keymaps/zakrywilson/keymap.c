05b3992fe8027a6136e44dd09556dd595c500149
Zach Wilson <zakrywilson@gmail.com>
2022-09-01 12:29:19 -0500

    Customized the OLED for master

--- keyboards/splitkb/kyria/keymaps/zakrywilson/keymap.c
+++ keyboards/splitkb/kyria/keymaps/zakrywilson/keymap.c
@@ -218,28 +218,30 @@ static void render_kyria_logo(void) {
   oled_write_raw_P(kyria_logo, sizeof(kyria_logo));
 }
 
-static void render_qmk_logo(void) {
-  static const char PROGMEM qmk_logo[] = {
-    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
-
-  oled_write_P(qmk_logo, false);
-}
-
 static void render_status(void) {
   // QMK Logo and version information
-  render_qmk_logo();
-  oled_write_P(PSTR("       Kyria rev1.3\n\n"), false);
+  oled_write_ln_P(PSTR("Kyria rev1.3\n"), false);
 
-  // Host Keyboard Layer Status
-  oled_write_P(PSTR("Layer: "), false);
-  switch (get_highest_layer(layer_state)) {
+  oled_write_ln_P(keymap_config.swap_lalt_lgui ? PSTR("System: Linux\n") : PSTR("System: Mac\n"), false);
+
+  oled_write_P(PSTR("Layout: "), false);
+  switch (get_highest_layer(default_layer_state)) {
     case _COLEMAK:
-      oled_write_P(PSTR("Colemak\n"), false);
+      oled_write_ln_P(PSTR("Colemak\n"), false);
       break;
     case _QWERTY:
-      oled_write_P(PSTR("Qwerty\n"), false);
+      oled_write_ln_P(PSTR("Qwerty\n"), false);
+      break;
+    default:
+      oled_write_P(PSTR("Undefined\n"), false);
+  }
+
+  // Host Keyboard Layer Status
+  oled_write_P(PSTR("Layer:  "), false);
+  switch (get_highest_layer(layer_state)) {
+    case _COLEMAK:
+    case _QWERTY:
+      oled_write_P(PSTR("Base\n"), false);
       break;
     case _SYM:
       oled_write_P(PSTR("Symbols\n"), false);
@@ -255,10 +257,10 @@ static void render_status(void) {
   }
 
   // Host Keyboard LED Status
-  led_t led_state = host_keyboard_led_state();
-  oled_write_P(led_state.num_lock ? PSTR("NUMLCK ") : PSTR("       "), false);
-  oled_write_P(led_state.caps_lock ? PSTR("CAPLCK ") : PSTR("       "), false);
-  oled_write_P(led_state.scroll_lock ? PSTR("SCRLCK ") : PSTR("       "), false);
+  /* led_t led_state = host_keyboard_led_state(); */
+  /* oled_write_P(led_state.num_lock ? PSTR("NUMLCK ") : PSTR("       "), false); */
+  /* oled_write_P(led_state.caps_lock ? PSTR("CAPLCK ") : PSTR("       "), false); */
+  /* oled_write_P(led_state.scroll_lock ? PSTR("SCRLCK ") : PSTR("       "), false); */
 }
 
 bool oled_task_user(void) {
