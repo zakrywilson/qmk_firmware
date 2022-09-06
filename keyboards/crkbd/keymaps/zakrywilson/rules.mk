953a9343b14910645ea64dba3939f7c99ccb77db
Zach Wilson <zakrywilson@gmail.com>
2022-09-06 12:38:39 -0500

    Disable things we aren't using

--- keyboards/crkbd/keymaps/zakrywilson/rules.mk
+++ keyboards/crkbd/keymaps/zakrywilson/rules.mk
@@ -1,6 +1,5 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
+MOUSEKEY_ENABLE = no
+RGBLIGHT_ENABLE = no
+OLED_ENABLE     = no
 LTO_ENABLE      = yes
 BOOTLOADER      = atmel-dfu
