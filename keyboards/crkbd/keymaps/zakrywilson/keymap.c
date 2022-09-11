8ee5277eaffb6587b5641c90ccc6ff419d3408ba
Zach Wilson <zakrywilson@gmail.com>
2022-09-10 22:50:55 -0500

    Removed the key logger

--- keyboards/crkbd/keymaps/zakrywilson/keymap.c
+++ keyboards/crkbd/keymaps/zakrywilson/keymap.c
@@ -247,34 +247,6 @@ void oled_render_layer_state(void) {
   }
 }
 
-char keylog_str[24] = {};
-
-const char code_to_name[60] = {
-  ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
-  'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-  '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-  'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
-  '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
-
-void set_keylog(uint16_t keycode, keyrecord_t *record) {
-  char name = ' ';
-  if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
-      (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
-  if (keycode < 60) {
-    name = code_to_name[keycode];
-  }
-
-  // Update keylog
-  snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d | %c",
-      record->event.key.row, record->event.key.col,
-      keycode, name);
-}
-
-void oled_render_keylog(void) {
-  oled_write(keylog_str, false);
-}
-
 void render_bootmagic_status(bool status) {
   // Show Ctrl-Gui Swap options
   static const char PROGMEM logo[][2][3] = {
@@ -304,17 +276,10 @@ bool oled_task_user(void) {
     oled_render_system_state();
     oled_render_layout_state();
     oled_render_layer_state();
-    oled_render_keylog();
   } else {
     oled_render_logo();
   }
   return false;
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-    set_keylog(keycode, record);
-  }
-  return true;
-}
 #endif // OLED_ENABLE
