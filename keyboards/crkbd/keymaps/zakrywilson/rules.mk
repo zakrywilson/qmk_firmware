3faad7bd877494574a4824c4f24183b915f6bea2
Zach Wilson <zakrywilson@gmail.com>
2022-09-10 22:35:40 -0500

    git commit --amend

--- keyboards/crkbd/keymaps/zakrywilson/rules.mk
+++ keyboards/crkbd/keymaps/zakrywilson/rules.mk
@@ -1,6 +1,7 @@
 MOUSEKEY_ENABLE = no
 RGBLIGHT_ENABLE = no
-OLED_ENABLE     = no
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
 BOOTLOADER      = atmel-dfu
 COMBO_ENABLE    = yes
