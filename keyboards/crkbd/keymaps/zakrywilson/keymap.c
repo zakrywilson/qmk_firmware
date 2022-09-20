f6a27b59f71286892ef56945febd62af1f76e685
Zach Wilson <zakrywilson@gmail.com>
2022-09-20 15:34:10 -0500

    Added some more ctrl

--- keyboards/crkbd/keymaps/zakrywilson/keymap.c
+++ keyboards/crkbd/keymaps/zakrywilson/keymap.c
@@ -13,6 +13,7 @@ enum layers {
 };
 
 #define CLMK     DF(_COLEMAK)     // Set the base (default) layer to Colemak (default)
+#define CTL_0    LCTL_T(KC_0)     // Left Ctrl when held, 0 when tapped
 #define CTL_CLN  LCTL_T(KC_COLON) // Left Opt when held, : when tapped
 #define DEL_LN   LCMD(KC_BSPC)    // Delete a line (backwards)
 #define DEL_WD   LOPT(KC_BSPC)    // Delete a word (backwards)
@@ -149,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX,  KC_TAB,    KC_7,    KC_8,    KC_9,KC_COMMA,                      KC_TILD, KC_UNDS, KC_LPRN, KC_RPRN,KC_COLON, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,    KC_0,    KC_4,    KC_5,    KC_6,  KC_DOT,                       KC_GRV, KC_MINS, KC_LBRC, KC_RBRC, KC_QUOT, XXXXXXX,
+      XXXXXXX,   CTL_0,    KC_4,    KC_5,    KC_6,  KC_DOT,                       KC_GRV, KC_MINS, KC_LBRC, KC_RBRC, KC_QUOT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, OPT_ENT,    KC_1,    KC_2,    KC_3,KC_COLON,                      KC_BSLS,  KC_EQL,   KC_LT,   KC_GT, KC_TRNS, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -161,7 +162,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       DEL_LN, KC_HOME,   KC_UP,  KC_END, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT,  KC_DEL,                      KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
+      XXXXXXX, KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT,  KC_DEL,                      KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT,      FN, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       DEL_WD,  KC_ENT, KC_PGUP, KC_PGDN, KC_TRNS, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -185,7 +186,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         CLMK,    QWTY,     MAC,     NIX,    LOCK, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, _______, _______, _______, _______, _______,                      KC_BRIU, KC_VOLU, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, _______, _______, _______, _______, _______,                      KC_BRIU, KC_VOLU, KC_MUTE, XXXXXXX, KC_TRNS, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                      KC_BRID, KC_VOLD, KC_MPLY, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
