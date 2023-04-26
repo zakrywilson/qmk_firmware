7416cf65638aad9531bc6ff2e7321b52cb31e879
Zach Wilson <zakrywilson@gmail.com>
2023-04-26 16:10:16 -0500

    Adds Space/Ctrl tap dance

--- keyboards/ferris/keymaps/zakrywilson/keymap.c
+++ keyboards/ferris/keymaps/zakrywilson/keymap.c
@@ -92,12 +92,21 @@ combo_t key_combos[] = {
 };
 
 
+enum tap_dances {
+  SPC_CTL = 0
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+  [SPC_CTL]  = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_LCTL)
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_Q,  KC_W, KC_F, KC_P, KC_G,    KC_J, KC_L, KC_U,    KC_Y,   KC_SCLN,
     KC_A,  KC_R, KC_S, KC_T, KC_D,    KC_H, KC_N, KC_E,    KC_I,   KC_O,
     OPT_Z, KC_X, KC_C, KC_V, KC_B,    KC_K, KC_M, KC_COMM, KC_DOT, NAV_SLSH,
-                 KC_LSFT, CMD_ESC,    SYM_BSPC, CTL_SPC
+             TD(SPC_CTL), CMD_ESC,    SYM_BSPC, CTL_SPC
   ),
 
   [_SYM] = LAYOUT(
