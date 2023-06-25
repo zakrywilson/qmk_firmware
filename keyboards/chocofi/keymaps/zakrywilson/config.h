6a5eaa78d0b4184b550e6627c615db96d07564b6
Zach Wilson <zakrywilson@gmail.com>
2023-06-24 19:22:53 -0500

    Adds zakrywilson chocofi keymap

---
+++ keyboards/chocofi/keymaps/zakrywilson/config.h
@@ -0,0 +1,45 @@
+#pragma once
+
+// Tapping term
+//
+// Source: https://github.com/qmk/qmk_firmware/blob/master/docs/tap_hold.md#tapping-term
+//
+// The crux of all of the following features is the tapping term setting. This
+// determines what is a tap and what is a hold. The exact timing for this to
+// feel natural can vary from keyboard to keyboard, from switch to switch, and
+// from key to key.
+#define TAPPING_TERM 200
+
+// Permissive hold
+//
+// Source: https://github.com/qmk/qmk_firmware/blob/master/docs/tap_hold.md#permissive-hold
+//
+// This makes tap and hold keys (like Layer Tap) work better for fast typists,
+// or for high TAPPING_TERM settings. If you press a dual-role key, tap another
+// key (press and release) and then release the dual-role key, all within the
+// tapping term, by default the dual-role key will perform its tap action.
+// If the PERMISSIVE_HOLD option is enabled, the dual-role key will perform its
+// hold action instead.
+#define PERMISSIVE_HOLD
+
+// Hold on other press
+//
+// Source: https://github.com/qmk/qmk_firmware/blob/master/docs/tap_hold.md#hold-on-other-key-press
+//
+// This mode makes tap and hold keys (like Layer Tap) work better for fast
+// typists, or for high TAPPING_TERM settings. Compared to the "permissive hold"
+// mode, this mode selects the hold action in more cases.
+// If you press a dual-role key, press another key, and then release the
+// dual-role key, all within the tapping term, by default the dual-role key will
+// perform its tap action. If the HOLD_ON_OTHER_KEY_PRESS option is enabled, the
+// dual-role key will perform its hold action instead.
+//
+// "Per key" requires implementing `get_hold_on_other_key_press` in keymap.c
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+
+// Reducing footprint
+// Source: https://docs.qmk.fm/#/squeezing_avr
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#define NO_ACTION_ONESHOT
+#define NO_MUSIC_MODE
